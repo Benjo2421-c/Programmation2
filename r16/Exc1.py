6d7331300e9533ab4d7849e5d7b6fd5315645ed4
Benjo2421 <2132056@cegepmontpetit.ca>
2024-03-26 17:21:26 -0400

    fini

--- r16/Exc1.py
+++ r16/Exc1.py
@@ -1,3 +1,5 @@
+import csv
+
 class Departement:
     def __init__(self, id, nom, budget_annuel) -> None:
         self.id = id
@@ -5,5 +7,34 @@ class Departement:
         self.budget_annuel = budget_annuel
 
 
-    @classmethod
-    def from_string(cls, )
+        
+class Achat:
+    def __init__(self, id, montant_total_achat, est_paye, equipements) -> None:
+        self.id = id
+        self.montant_total_achat = montant_total_achat
+        self.est_paye= est_paye
+        self.equipement = equipements
+    
+    def calculer_total_estimer():
+        pass
+    def payer():
+        pass
+
+
+class Equipement:
+    def __init__(self, id, nom_equipement, cie, modele, qty, prix) -> None:
+        self.id = id
+        self.nom_equipement = nom_equipement
+        self.cie = cie
+        self.modele = modele
+        self.qty = qty
+        self.prix = prix
+
+
+equipements = []
+with open ("r16/AchatsDepartement2023.csv", "r") as fichier_lu:
+        csv_reader = csv.reader(fichier_lu, delimiter=";")
+        next (csv_reader)
+        for line in csv_reader:
+            eq = Equipement(line[0],line[1], line[2], line[3], line[4], line[5])
+            
